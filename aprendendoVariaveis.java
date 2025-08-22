538e92b386880a49fb5e4b1c94fa98221a5143be
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-22 20:05:12 -0300

    Create aprendendoVariaveis.java

---
+++ aprendendoVariaveis.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+      
+      //aprendendo a usar as variáveis
+
+       int points = 22;
+       int groupOfstudents = 20;
+       int numberOfStudents = 10;
+       int aCaixaSalaDeEstar = 15;
+       int aCaixaMasterBedroom = 45;
+       int oBanheiro = 10;
+
+       	System.out.println(oBanheiro);
+           System.out.println(points);
+           System.out.println(groupOfstudents);
+           System.out.println(numberOfStudents);
+           System.out.println(aCaixaSalaDeEstar);
+           System.out.println(aCaixaMasterBedroom);
+
+
+    }
+}
