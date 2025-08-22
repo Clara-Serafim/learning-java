8a2862733c839b12a9f36c6c89296361f007c387
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-22 20:36:08 -0300

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+       //aprendendo as diferenças dos tipos de dados
+       
+       //int
+       int age = 19;
+       int cokkies = 0;
+
+       //double
+       double pi = 3.14;
+       double megapixels = 34.674423;
+
+       //boolean
+       boolean favColorBlue = true;
+       boolean subscribed = false;
+
+       //char
+       char theFirstLetter = 'a';
+       char hashtag = '#';
+
+       //string
+       String wordleWord = "TAKER";
+       String artist = "A$ap rocky";
+
+       //praticas
+      String name = "Serafim"; // aspas duplas
+      String state = "Ceará";
+      char gender = 'F'; //aspas simples
+      int ageName = 16; //sem aspas
+      boolean loveChocolate = true; //sem aspas
+
+        System.out.println(name);
+		System.out.println(state);
+		System.out.println(gender);
+		System.out.println(ageName);
+		System.out.println(loveChocolate);
+
+    }
+}
