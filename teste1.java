ddac94a3dd5875a924ff357d1316ff6908249bc3
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-22 19:42:40 -0300

    Create teste1.java

---
+++ teste1.java
@@ -0,0 +1,17 @@
+package teste12025;
+
+public class teste1 {
+
+	public static void main(String[] args) {
+		String msg1 = new String("Olá, mundo!");
+		String msg2 = "Olá, mundo!";
+		String msg3 = "";
+		
+		int i =10; double d = 3.0;
+		
+		String msg4 = msg1 + "--" + i + "--" + d;
+		System.out.println(msg4);
+	
+	}
+
+}
