39431e6488a98779fec76247796c22bdc6e46c14
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-23 09:27:33 -0300

    Create operadores.java

---
+++ operadores.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) { 
+
+int result1 = 8 + 2;     // result is now 10
+int result2 = 8 - 2;     // result is now 6
+int result3 = 8 * 2;     // result is now 16
+int result4 = 8 / 2;     // result is now 4
+int result5 = 8 % 2;     // result is now 0
+
+
+System.out.println(result1);
+System.out.println(result2); 
+System.out.println(result3);
+System.out.println(result4); 
+System.out.println(result5); 
+        
+    }
+}
