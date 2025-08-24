c0f3094d0fc8878fc0ad556eab81efb287bbb4e0
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-24 09:21:43 -0300

    Create if.java

---
+++ if.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+   System.out.println("No one takes Alan's piece of cheese and gets away with it!");
+    int oats = 2; //Aveia
+
+    if (oats > 0) {
+      System.out.println("Let´s make oatmeal today!"); //farinha de aveia
+    }
+       }
+          }
