031ff68220e5987d76acca3f8ff250c9ea7ee0fe
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-24 09:45:38 -0300

    Create else2.java

---
+++ else2.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        boolean isCatOwner = false; //change here for a different answer
+        if (isCatOwner)  {
+            System.out.println("20% de desconto em itens selecionados para gatos com o código MEOW2025");
+        } else {
+            System.out.println("Bem-vindo à loja Pets Pets Pets!");
+        }
+    }
+}
