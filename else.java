8b97f4348096d15e28ad87ff33651dd3f64b5737
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-24 09:35:46 -0300

    Create else.java

---
+++ else.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int spiceLevel = 4; // change here for different answer
+        boolean isSpicy = spiceLevel > 3;
+
+        if (isSpicy) {
+            System.out.println("Careful! this is spicy fried rice.");
+        } else {
+            System.out.println("Spice tolerance where? ");
+        }
+    }
+}
