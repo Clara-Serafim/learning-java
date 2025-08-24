55ba86a81071d0780fdcb0621fa261ea5f3968dc
Clara <mariaclaraserafimsilva09@gmail.com>
2025-08-24 09:53:46 -0300

    Create else3.java

---
+++ else3.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int onlinePrice = 11;
+        int storePrice = 11;
+
+        if (onlinePrice == storePrice) {
+             System.out.println("The prices are the same!");
+}
+         int temp = 58; //change here
+
+         if (temp < 40) {
+            System.out.println("It may be snowing! ❄️"); 
+        } else if (temp < 80) {
+            System.out.println("Enjoy the weather! 👌");
+        } else {
+             System.out.println("Okay... It is getting a little hot in here. 🔥"); 
+}
+    }
+}
